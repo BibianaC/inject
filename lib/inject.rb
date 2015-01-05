f3215c38730ee37b315e55a37eefd75d155a5d57
bibiana <bcristofol@gmail.com>
2015-01-05 12:05:10 +0000

    Created one method and deleted the rest

--- lib/inject.rb
+++ lib/inject.rb
@@ -1,45 +1,27 @@
 class Array
 
-  def block_inject(&block)
+  def my_inject(init_or_sym = nil, sym = nil, &block)
+    if sym
+      memo = init_or_sym
+      block = Proc.new {|memo, num| memo.send(sym, num)}
+    elsif init_or_sym
+      if init_or_sym.class == Symbol
+        sym = init_or_sym
+        block = Proc.new {|memo, num| memo.send(sym, num)}
+      else
+        memo = init_or_sym
+      end
+    else
+      raise "Symbol or block must be passed" unless block
+    end
+
     copy = self.dup
-    memo = self[0]
-    copy.shift
+    if memo.nil?
+      memo = self[0]
+      copy.shift
+    end
     copy.each {|num| memo = block.call(memo, num)}
     memo
-  end
-
-  def sym_inject(sym)
-    (self).reduce(sym)
-  end
-
-  def init_inject(init)
-    copy = self.dup
-    memo = init
-    copy.each {|num| memo = yield(memo, num)}
-    memo
-  end
-
-  def init_sym_inject(init, sym)
-    (self).reduce(init, sym)
-  end
-
-  def my_inject(*args, &block)
-    if args.length == 0
-      return block_inject(&block)
-    end
-
-    if args.length == 1 && args[0].class == Symbol
-      return sym_inject(args[0])
-    end
-
-    if args.length == 1 && args[0].class == Fixnum
-      return init_inject(args[0], &block)
-    end
-
-    if args.length == 2
-      return init_sym_inject(args[0], args[1])
-    end
-    raise "invalid invocation"
   end
 
 end
